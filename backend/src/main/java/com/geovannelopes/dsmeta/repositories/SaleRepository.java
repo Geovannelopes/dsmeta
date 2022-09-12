0866aba071ca468e941ce305348f6b6f9a83b154
Geovanne Lopes <geovanneblopes2@gmail.com>
2022-09-12 11:31:36 -0300

    API test

---
+++ backend/src/main/java/com/geovannelopes/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.geovannelopes.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.geovannelopes.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
